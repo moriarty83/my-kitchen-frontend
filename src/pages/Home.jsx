cfd19e958f021f551520c9b22ba57ec676253188
moriarty83 <moriarty83@gmail.com>
2022-01-07 16:38:54 -0500

    splash dropdown menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,17 +30,17 @@ function Home (props) {
                 <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                   <div className="flex items-center justify-between w-full md:w-auto">
                     <a href="#">
-                      <span className="sr-only">Workflow</span>
+                      <span className="sr-only"></span>
                       <img
                         className="h-10 w-auto sm:h-20"
                         src="/mykitchen_logo1_black.png"
                       />
                     </a>
                     <div className="-mr-2 flex items-center md:hidden">
-                      {/* <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
                         <span className="sr-only">Open main menu</span>
                         <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button> */}
+                      </Popover.Button>
                     </div>
                   </div>
                 </div>
