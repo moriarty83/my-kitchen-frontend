073b872c33dabe0b6a5ca4e7499b4c21a1da2c65
moriarty83 <moriarty83@gmail.com>
2022-02-07 21:33:37 -0500

    z on account nav z-30

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -99,7 +99,7 @@ function Nav (props){
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-20">
+                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-30">
                       <Menu.Item>
                         {({ active }) => (
                           <a
