8886005b09f55103e487d46ab5e78b818835ab9a
moriarty83 <moriarty83@gmail.com>
2022-02-07 21:27:29 -0500

    search on enter

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -39,6 +39,12 @@ function Search ({menuOption, setQuery}){
         setSearchType(type)
     }
 
+    const handleKeyDown = (event)=>{
+        if(event.key === 'Enter'){
+            handleSubmit(event)
+          }
+    }
+
     const handleSubmit = (event)=>{
         const type = selected.name.toLowerCase()
         if(formData.search === ""){
@@ -129,9 +135,9 @@ function Search ({menuOption, setQuery}){
         </Listbox>
 
     <div className="w-64 h-10 pl-3 pr-2 bg-white border rounded-full flex justify-between items-center relative m-4">
-    <input type="search" onChange={handleChange} name="search" id="search" placeholder="Search" required
+    <input type="search" onKeyDown={handleKeyDown} onChange={handleChange} name="search" id="search" placeholder="Search" required
             className="appearance-none w-full outline-none focus:outline-none active:outline-none"/>
-    <button type="submit" onClick={()=>{handleSubmit()}} className="ml-1 outline-none focus:outline-none active:outline-none">
+    <button type="submit" onK onClick={()=>{handleSubmit()}} className="ml-1 outline-none focus:outline-none active:outline-none">
         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
             viewBox="0 0 24 24" className="w-6 h-6">
         <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
